7e611e71ad98950e5d71dd6410df7b299730a19a
King-James21 <hc9b7tzww6@privaterelay.appleid.com>
2026-05-09 18:04:31 +0000

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 from datetime import date
 from streamlit_gsheets import GSheetsConnection
-conn = st.connection("gsheets", type=GSheetsConnection)
+conn = st.connection("gsheets", type=GSheetsConnection, spreadsheet="https://docs.google.com/spreadsheets/d/1-yvBDHWkA__r3lsY-yGEcxb1kWz608yzZkIVQuzch4o/edit#gid=0")
 st.header("Record New Sale")
 
 # --- FORM SECTION ---
